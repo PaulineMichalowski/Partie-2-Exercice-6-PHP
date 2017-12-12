f105105852694a9d3aab7ebbbae51e5c2bed642f
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 13:44:53 +0100

    Création de l'exercice 6

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+      $monAge = 13;
+      if($monAge >= 18) {
+        echo 'Tu es majeur !';
+      }
+      else {
+        echo 'Tu n\'es pas majeur';
+      }
+     ?>
+  </body>
+</html>
